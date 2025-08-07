b0477759a41d973fc7d2767d5c12118b95dcd914
Roy Steenwijk <steenwijkhawk@gmail.com>
2025-08-07 21:21:13 +0200

    Initial Commit

---
+++ Models/StandardPage.cs
@@ -0,0 +1,9 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace _20R_HomePage.Models;
+
+[PageType(Title = "Standard page")]
+public class StandardPage  : Page<StandardPage>
+{
+}
