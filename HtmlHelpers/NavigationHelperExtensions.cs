0cbd8d931e2f755df29282c3f498233363e38452
Roy Steenwijk <steenwijkhawk@gmail.com>
2025-08-07 22:35:17 +0200

    working dropdowns

---
+++ HtmlHelpers/NavigationHelperExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Piranha.Models;
+
+namespace _20R_HomePage.HtmlHelpers
+{
+    public static class NavigationHelperExtensions
+    {
+        public static bool ThisOrChildIsActive(this IHtmlHelper htmlHelper, SitemapItem current, StructureItem<Sitemap, SitemapItem> childItems)
+        {
+
+
+            return true;
+        }
+    }
+}
